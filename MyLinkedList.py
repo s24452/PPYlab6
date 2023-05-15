5d2a6a676c57e64bbc7b312b1df7153253f6859b
s24452 <s24452@pjwstk.edu.pl>
2023-05-15 20:36:16 +0200

    .

--- MyLinkedList.py
+++ MyLinkedList.py
@@ -1,18 +1,75 @@
-class MyLinkedList:
+class Element:
     def __init__(self, data=None, nextE=None):
         self.data=data
         self.nextE=nextE
+class MyLinkedList:
+    def __init__(self):
+        self.head=None
+        self.tail=None
+        self.size=0
 
 
     def __str__(self):
-        return str(self.head)+", tail"+str(self.tail)+", size= "+str(self.size)
-
+        #return str(self.head)+", tail"+str(self.tail)+", size= "+str(self.size)
+        if self.size==0:
+          return "Pusta lista"
+        obecna=self.head
+        elementy=[]
+        while obecna is not None:
+            elementy.append(str(obecna.data))
+            obecna=obecna.nextE
+        return " ".join(elementy)
     def get(self, e):
-        pass
-    def delete(selfself, e):
-        pass
-    def append(selfself, e, func=None):
-        pass
+        obecna=self.head
+        while obecna is not None:
+            if obecna.data==e:
+                return obecna.data
+            obecna=obecna.nextE
+        return None
+    def delete(self, e):
+        if self.head is None:
+            return
+        if self.head.data==e:
+            self.head=self.head.nextE
+            self.size-=1
+            return
+        obecna=self.head
+        while obecna.nextE is not None:
+            if obecna.nextE.data==e:
+                obecna.nextE=obecna.nextE.nextE
+                if obecna.nextE is None:
+                    self.tail=obecna
+                self.size-=1
+                return
+            obecna=obecna.nextE
+
+    def append(self, e, func=None):
+        nowy_ele=Element(data=e)
+
+        if self.head is None:
+            self.head=nowy_ele
+            self.tail=nowy_ele
+        else:
+            if func is None:
+                func=lambda a,b:a<=b
+
+                obecna=self.head
+                poprzednia=None
+                while obecna is not None and func(obecna.data,e):
+                    poprzednia=obecna
+                    obecna=obecna.nextE
+
+                if poprzednia is None:
+                    nowy_ele.nextE=self.head
+                    self.head=nowy_ele
+                else:
+                    poprzednia.nextE=nowy_ele
+                    nowy_ele.nextE=obecna
+
+                if obecna is None:
+                    self.tail=nowy_ele
+            self.size+=1
+
 
 class Lista:
     def __init__(self):
--- main.py
+++ main.py
@@ -1,32 +1,43 @@
 import Stos
 import Studenci
-stos = Stos.Stos(3)
-print(stos)
-stos.push(5)
-print(stos)
-stos.pop()
-print(stos)
-stos.pop()
+from MyLinkedList import MyLinkedList
 
-student=Studenci.Student("ala@gmail.com","Ala","Kot")
-student2=Studenci.Student("janK@gmail.com","Jan","Kowalski")
-slownik={}
-slownik[0]=student
-slownik[1]=student2
+lista=MyLinkedList()
+lista.append(15)
+lista.append(4)
+lista.append(17)
+lista.append(6)
 
-print(slownik)
-
-print(Studenci.Student.compareStudentName(student, student2))
-print(Studenci.Student.compareStudentSurname(student, student2))
-
-def alphabeticGreater(x,y,func):
-    return func(x,y)
-print(alphabeticGreater(student,student2,Studenci.Student.compareStudentName))
-print(alphabeticGreater(student,student2,Studenci.Student.compareStudentSurname))
-
-#list.append(e,Studenci.Student.compareStudentName)
-
-email, name, surname = student.getPersonalData()
-print(email, name, surname)
-
+print(lista)
+def sortowanie(a,b):
+    return abs(a)>=abs(b)
+# stos = Stos.Stos(3)
+# print(stos)
+# stos.push(5)
+# print(stos)
+# stos.pop()
+# print(stos)
+# stos.pop()
+#
+# student=Studenci.Student("ala@gmail.com","Ala","Kot")
+# student2=Studenci.Student("janK@gmail.com","Jan","Kowalski")
+# slownik={}
+# slownik[0]=student
+# slownik[1]=student2
+#
+# print(slownik)
+#
+# print(Studenci.Student.compareStudentName(student, student2))
+# print(Studenci.Student.compareStudentSurname(student, student2))
+#
+# def alphabeticGreater(x,y,func):
+#     return func(x,y)
+# print(alphabeticGreater(student,student2,Studenci.Student.compareStudentName))
+# print(alphabeticGreater(student,student2,Studenci.Student.compareStudentSurname))
+#
+# #list.append(e,Studenci.Student.compareStudentName)
+#
+# email, name, surname = student.getPersonalData()
+# print(email, name, surname)
+#
 
